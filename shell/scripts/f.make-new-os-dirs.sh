1196b6405f98270a6b05fd7e46720776ff702891
fulara <ntszar@gmail.com>
2017-07-15 00:25:56 +0200

    Further updating

--- shell/scripts/f.make-new-os-dirs.sh
+++ shell/scripts/f.make-new-os-dirs.sh
@@ -1,3 +1,4 @@
 mkdir -p ~/repos
 mkdir -p ~/progsy
 mkdir -p ~/tmp
+
--- shell/sourced.sh
+++ shell/sourced.sh
@@ -1,6 +1,4 @@
-THIS_SCRIPT_PATH=$(this-script-path)
-
-source $THIS_SCRIPT_PATH/env-variables.sh
+source $ENV_SCRIPT_PATH/env-variables.sh
 
 function rebash { source ~/.profile; }
 function grep { command grep --color=auto --exclude-dir='.svn' --exclude-dir='.git'  "$@"; }
@@ -8,6 +6,5 @@ function grep { command grep --color=auto --exclude-dir='.svn' --exclude-dir='.g
 function f.lookup { find . -iname "$1" -not -path '*.git*' -not -path '*.svn'; } 
 function f.wlookup { grep -R "$@" .; }
 
-THIS_SCRIPT_PATH=$(this-script-path)
-alias f.cdenv="cd $THIS_SCRIPT_PATH"
+alias f.cdenv="cd $ENV_SCRIPT_PATH"
 function f.cdrepo { cd $MYHOME/repos; }
